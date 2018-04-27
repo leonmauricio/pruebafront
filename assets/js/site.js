0df2037aa3822c633b633683dcbde79433b2f9b5
Mauricio <mmunoz@argin.com.ar>
2018-04-27 19:27:11 -0300

    Solved JS Issues

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,37 +4,30 @@ $( document ).ready(function() {
     var price = $(".price_editable"),
     	meters_price = $(".result");
     	heart = $(".fa-heart");
+        price_storage = localStorage.getItem("price_editable");
+        meters_storage = localStorage.getItem("meters_result");
+        fav_storage = localStorage.getItem("favourite");
 
-    if (!localStorage.getItem("price_editable") || !localStorage.getItem("meters_result")){
+    if (!price_storage || !meters_storage){
     	price.text('1,400,000');
     	meters_price.text(calculateMetersPrice(price.text().replace(/[^0-9.]/g, "")));
     } else {
-    	price.text(numberWithCommas(localStorage.getItem("price_editable")));
-    	meters_price.text(numberWithCommas(localStorage.getItem("meters_result")));
+    	price.text(numberWithCommas(price_storage));
+    	meters_price.text(numberWithCommas(meters_storage));
     };
 
     price.on( "focusout", function(){
-    	price_clean = parseFloat(price.text().replace(/[^0-9.]/g, ""));
+    	var price_clean = parseFloat(price.text().replace(/[^0-9.]/g, ""));
     	price.text(numberWithCommas(price_clean));
 
-    	meters_result = numberWithCommas(calculateMetersPrice(price_clean));
+    	var meters_result = numberWithCommas(calculateMetersPrice(price_clean));
     	meters_price.text(meters_result);
 
     	localStorage.setItem("price_editable", price_clean);
-    	localStorage.setItem("meters_result", meters_result);
+        localStorage.setItem("meters_result", meters_result);
+    	localStorage.removeItem("favourite");
     });
 
-    function numberWithCommas(number) {
-	    var parts = number.toString().split(".");
-	    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	    return parts.join(".");
-	}
-
-	function calculateMetersPrice(price){
-		meters_clean = parseFloat($(".meters").text().replace(/[^0-9.]/g, ""));
-		meters_result = price / meters_clean;
-		return numberWithCommas(meters_result.toFixed(2));
-	}
 
     $('.owl-carousel').owlCarousel({
         loop:true,
@@ -43,19 +36,33 @@ $( document ).ready(function() {
         items:1,
     });
 
-    if (localStorage.getItem("red_heart") == null){
-        heart.css("color", "#000000");
+    if (localStorage.getItem("favourite") == 1){
+        heart.css("color", "#ff0000");
     }
 
     heart.on("click", function(){
-        if (localStorage.getItem("red_heart") == null){
+        if (!localStorage.getItem("favourite")){
             heart.css("color", "#ff0000");
-            localStorage.setItem("red_heart", "red");
+            localStorage.setItem("favourite", 1);
         } else {
             heart.css("color", "#000000");
-            localStorage.removeItem("red_heart");
+            localStorage.removeItem("favourite");
+            console.log(fav_storage);
         }
-        console.log(localStorage.getItem("red_heart", "red"));
-    })
+    });
+
+    console.log(fav_storage);
 
 });
+
+function numberWithCommas(number) {
+    var parts = number.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
+function calculateMetersPrice(price){
+	var meters_clean = parseFloat($(".meters").text().replace(/[^0-9.]/g, ""));
+	var meters_result = price / meters_clean;
+	return numberWithCommas(meters_result.toFixed(2));
+}
